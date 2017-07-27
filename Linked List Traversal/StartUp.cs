ea0de3f4af5cdaabaa1f22832939d00a7325d1b5
Mario Slavov <hellsflash@gmail.com>
2017-07-27 06:17:09 +0300

    Solved all problems from Iterators and Comparators Exercise

---
+++ Linked List Traversal/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        int numberOfCommands = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCommands; i++)
+        {
+            string[] commandInfo = Console.ReadLine().Split();
+
+            string command = commandInfo[0];
+            int number = int.Parse(commandInfo[1]);
+
+            switch (command)
+            {
+                case "Add":
+                    linkedList.Add(number);
+                    break;
+
+                case "Remove":
+                    linkedList.Remove(number);
+                    break;
+            }
+        }
+
+        Console.WriteLine(linkedList.Count);
+        Console.WriteLine(string.Join(" ", linkedList));
+    }
+}
